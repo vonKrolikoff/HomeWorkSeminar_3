1c1ff3c1c27e19e3133347aabe2bf15ea0a15b4e
von_Krolikoff <milkbrother@yandex.ru>
2022-06-19 18:07:41 +0300

    +EX02 в двух версиях

--- EX02/Program.cs
+++ EX02/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+int x1 = Input ("XA - ");
+int y1 = Input ("YA - ");
+int z1 = Input ("ZA - ");
+
+int x2 = Input ("XB - ");
+int y2 = Input ("YB - ");
+int z2 = Input ("ZB - ");
+
+int x = Result (x1, x2);
+int y = Result (y1, y2);
+int z = Result (z1, z2);
+
+Console.WriteLine (Math.Sqrt(x+y+z));
+
+int Result (int a, int b)
+{
+    return (b - a)*(b - a);
+}
+
+int Input (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ EX02v2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+Console.WriteLine("Введите координаты первой точки - ");
+int x1 = Input ("X - ");
+int y1 = Input ("Y - ");
+int z1 = Input ("Z - ");
+
+Console.WriteLine("Введите координаты второй точки - ");
+int x2 = Input ("X - ");
+int y2 = Input ("Y - ");
+int z2 = Input ("Z - ");
+
+int x = (x2-x1)*(x2-x1);
+int y = (y2-y1)*(y2-y1);
+int z = (z2-z1)*(z2-z1);
+
+Console.WriteLine (Math.Sqrt(x+y+z));
+
+int Input (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
