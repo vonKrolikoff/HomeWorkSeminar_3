1906ca0aea268580b1ac4cdb296a51ca69c219c8
von_Krolikoff <milkbrother@yandex.ru>
2022-06-19 16:24:19 +0300

    +EX03

---
+++ EX03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите числo N - ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    int result = Func(count);
+    count=count+1;
+    Console.Write($"{result} ");
+}
+
+int Func (int a)
+{
+    return a * a * a;
+}
